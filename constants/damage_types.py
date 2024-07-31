a86b12f1318a793a92b82891f0a674bbf7c22937
Luke Huisman <huisman.l@yahoo.com>
2024-07-31 20:44:24 +0200

    Added damage types enum

---
+++ constants/damage_types.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class DamageTypes(Enum):
+    Acid = 1
+    Bludgeoning = 2
+    Cold = 3
+    Fire = 4
+    Force = 5
+    Lightning = 6
+    Necrotic = 7
+    Piercing = 8
+    Poison = 9
+    Psychic = 10
+    Radiant = 11
+    Slashing = 12
+    Thunder = 13
