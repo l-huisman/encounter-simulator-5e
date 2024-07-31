2eb6b17d36f454fc428bc6990284d690b282c4af
Jeppie <jespervdven5@gmail.com>
2024-07-31 21:54:07 +0200

    weapon class

---
+++ models/weapon.py
@@ -0,0 +1,24 @@
+from constants.damage_type import DamageType
+from constants.item_rarity import ItemRarity
+from constants.weapon_property import WeaponProperty
+from constants.weapon_type import WeaponType
+from models.dice import Dice
+
+
+class Weapon:
+    def __init__(self, name, type: WeaponType, dice: Dice, damage_type: DamageType, 
+                 rarity: ItemRarity, properties: WeaponProperty, range: tuple[int] | int, weight: float) -> None:
+        self.name = name
+        self.type = type
+        self.dice = dice
+        self.damage_type = damage_type
+        self.rarity = rarity
+        self.properties = properties
+        self.range = range
+        self.weight = weight
+
+    def use(self):
+        return self.dice.roll()
+        
+
+    
